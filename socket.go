098f1012182abc5790b223ae532965553b7d7b76
Karan Misra <kidoman@gmail.com>
2014-11-22 05:22:27 +0530

    no need to maintain the remote address separately

--- socket.go
+++ socket.go
@@ -9,20 +9,15 @@ import (
 )
 
 type socket struct {
-	raddr *net.UDPAddr
-	conn  *net.UDPConn
+	conn net.Conn
 }
 
 func newSocket(addr string) (*socket, error) {
-	raddr, err := net.ResolveUDPAddr("udp4", addr)
+	conn, err := net.DialTimeout("udp4", addr, time.Second)
 	if err != nil {
 		return nil, err
 	}
-	conn, err := net.ListenUDP("udp4", nil)
-	if err != nil {
-		return nil, err
-	}
-	return &socket{raddr, conn}, nil
+	return &socket{conn}, nil
 }
 
 func (s *socket) close() {
@@ -30,29 +25,28 @@ func (s *socket) close() {
 }
 
 func (s *socket) send(payload []byte) error {
-	glog.V(1).Infof("steam: sending %v bytes payload to %v", len(payload), s.raddr)
-	glog.V(2).Infof("steam: sending payload to %v: %X", s.raddr, payload)
-	n, err := s.conn.WriteToUDP(payload, s.raddr)
+	glog.V(1).Infof("steam: sending %v bytes payload to %v", len(payload), s.conn.RemoteAddr())
+	glog.V(2).Infof("steam: sending payload to %v: %X", s.conn.RemoteAddr(), payload)
+	n, err := s.conn.Write(payload)
 	if err != nil {
 		return err
 	}
 	if n != len(payload) {
 		return errors.New("steam: could not send full request to server")
 	}
-
 	return nil
 }
 
 func (s *socket) receivePacket() ([]byte, error) {
-	glog.V(1).Infof("steam: trying to recieve bytes from %v", s.raddr)
+	glog.V(1).Infof("steam: trying to recieve bytes from %v", s.conn.RemoteAddr())
 	var buf [1500]byte
 	s.conn.SetReadDeadline(time.Now().Add(1 * time.Second))
-	n, _, err := s.conn.ReadFromUDP(buf[:])
+	n, err := s.conn.Read(buf[:])
 	if err != nil {
 		return nil, err
 	}
-	glog.V(1).Infof("steam: received %v bytes from %v", n, s.raddr)
-	glog.V(2).Infof("steam: received payload %v: %X", s.raddr, buf[:n])
+	glog.V(1).Infof("steam: received %v bytes from %v", n, s.conn.RemoteAddr())
+	glog.V(2).Infof("steam: received payload %v: %X", s.conn.RemoteAddr(), buf[:n])
 	return buf[:n], nil
 }
 
