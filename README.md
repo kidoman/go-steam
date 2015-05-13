92552e4412d73d40cf2072f57dcba6dd77af133d
Karan Misra <kidoman@gmail.com>
2015-05-13 08:23:34 +0530

    add steam cmd line utility

--- README.md
+++ README.md
@@ -12,6 +12,12 @@
 go get github.com/sostronk/go-steam
 ```
 
+To use the build in `steam` utility, install it using:
+
+```
+go get -u github.com/sostronk/go-steam/cmd/steam
+```
+
 ## License
 
 This code is free software; you can redistribute it and/or modify it under the terms of the MIT License. A copy of this license can be found in the included LICENSE file.
---
+++ cmd/steam/rcon.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/codegangsta/cli"
+	"github.com/sostronk/go-steam"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func init() {
+	registerCommand(&rconCommand)
+}
+
+var rconCommand = cli.Command{
+	Name:  "exec",
+	Usage: "exec a command on server",
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "forever, f",
+			Usage: "exec forever",
+		},
+		cli.DurationFlag{
+			Name:  "delay, d",
+			Usage: "time to wait between successive execs",
+			Value: 1 * time.Second,
+		},
+	},
+	Action: func(c *cli.Context) {
+		addr := c.GlobalString("addr")
+		if addr == "" {
+			fmt.Println("please provide the address to exec command on")
+			os.Exit(1)
+		}
+
+		if !c.Args().Present() {
+			fmt.Println("please provide the command to exec")
+			os.Exit(1)
+		}
+
+		cmd := c.Args().First()
+
+		fmt.Printf("Password: ")
+		password, err := terminal.ReadPassword(0)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not read password: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println()
+
+		o := &steam.ConnectOptions{
+			RCONPassword: string(password),
+		}
+		server, err := steam.Connect(addr, o)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not connect to server %v: %v\n", addr, err)
+			os.Exit(1)
+		}
+		defer server.Close()
+
+		for {
+			resp, err := server.Send(cmd)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "could not exec %q on %v: %v\n", cmd, addr, err)
+				os.Exit(1)
+			}
+
+			fmt.Println(resp)
+
+			if !c.Bool("forever") {
+				return
+			}
+
+			time.Sleep(c.Duration("delay"))
+		}
+	},
+}
